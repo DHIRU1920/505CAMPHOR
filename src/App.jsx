34fbcc4335304f1ad16e54dfc8cb818f1cf0a7f3
Dhiraj Jadhav <134361570+DHIRU1920@users.noreply.github.com>
2025-01-02 23:30:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import ContactForm from "./components/ContactForm";
 import AboutPage from "./components/AboutPage";
 import FaqComponent from "./components/FaqComponent";
 import Testimonials from "./components/Testimonials";
-import Footer from "./components/Footer";
 import Introduction from "./components/Introduction";
 import MarqueeHeader from "./components/MarqueeHeader";
 import ProductGallery from "./components/ProductGallery";
@@ -29,7 +28,6 @@ function App() {
           <Route path="/testimonials" element={<Testimonials />} />
         </Routes>
       </main>
-      <Footer />
       <WhatsAppButton />
     </Router>
   );
